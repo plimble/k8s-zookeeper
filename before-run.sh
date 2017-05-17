7a0104c2f80e6be128e1f4c78c42db28d49875c4
Witoo Harianto <witoo@plimble.com>
2017-05-18 03:29:01 +0700

    add replica

--- before-run.sh
+++ before-run.sh
@@ -2,7 +2,16 @@
 
 T_HOSTNAME=$(hostname)
 T_ID=$( echo ${T_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
+T_SERVICE=$( echo ${T_HOSTNAME} | cut -d "-" -f1 )
 
 ZOOKEEPER_SERVER_ID=$T_ID
+
+if [[ -n "${ZOOKEEPER_REPLICAS-}" ]]
+then
+  for (( i=0; i<=$ZOOKEEPER_REPLICAS-1; i++ ))
+  do
+    ZOOKEEPER_SERVERS="${ZOOKEEPER_SERVERS}${T_SERVICE}-${i}:2888:3888;"
+  done
+fi
 
 sh /etc/confluent/docker/run
