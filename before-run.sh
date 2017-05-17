46a852d0ceee0f7784a763ce516b4cc38964501b
Witoo Harianto <witoo@plimble.com>
2017-05-18 03:53:57 +0700

    change loop

--- before-run.sh
+++ before-run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 T_HOSTNAME=$(hostname)
 T_ID=$( echo ${T_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
@@ -8,9 +8,10 @@ ZOOKEEPER_SERVER_ID=$T_ID
 
 if [[ -n "${ZOOKEEPER_REPLICAS-}" ]]
 then
-  for (( i=0; i<=$ZOOKEEPER_REPLICAS-1; i++ ))
-  do
-    ZOOKEEPER_SERVERS="${ZOOKEEPER_SERVERS}${T_SERVICE}-${i}:2888:3888;"
+  i=0
+  while [ "$i" -lt $ZOOKEEPER_REPLICAS ]; do
+      ZOOKEEPER_SERVERS="${ZOOKEEPER_SERVERS}${T_SERVICE}-${i}:2888:3888;"
+      i=$(( i + 1 ))
   done
 fi
 
